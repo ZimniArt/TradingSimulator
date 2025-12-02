580d9d7f9f94f237cb8cd41081be07b0a6908f80
zimniart-admin <zimni.art@gmail.com>
2025-12-03 00:42:45 +0300

    nore planning

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 fn main() {
-    
+    //raw data
+
+    //loop
+
+    //visualisation
+
 }
+
+
+fn _behavior_1_buy(){
+    //prpbably buy at the start, sell at the end
+}
+
+fn _transaction(){
+    // probably will have a commission per every operation
+}
+
+
 // import raw data: date, average\midian price of the day
 
 //money in
