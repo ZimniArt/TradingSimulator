d92390d1441d9fc746eb62acb7e4d7b228b03f93
zimniart-admin <zimni.art@gmail.com>
2025-12-02 23:35:23 +0300

    pkan

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    
 }
+// import raw data: date, average\midian price of the day
+
+//money in
+
+//siimulation loop
+//behavior
+
+//graphic visualisation of data, stock prices\ money
+//result. money out
