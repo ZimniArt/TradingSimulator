d1b365fee63faef8b7a1897645b80b3ee72ff55f
zimniart-admin <zimni.art@gmail.com>
2025-12-06 14:51:51 +0300

    simulate several strategies

--- src/main.rs
+++ src/main.rs
@@ -16,16 +16,22 @@ struct DataPoint{
 }
 
 #[derive(Debug)]
+#[derive(Clone)]
 struct AccountBalancePoint{
     stocks_holding: u32,
     cash_ammount:f64,
 }
+
 trait Strategy {
 }
-struct LoopBuySellStrategy{}
+#[derive(Debug)]
+struct StategyLoopBuySell{}
+impl Strategy for StategyLoopBuySell {}
 
-impl Strategy for LoopBuySellStrategy {
-}
+#[derive(Debug)]
+struct StrategyStartBuyEndSell{}
+impl Strategy for StrategyStartBuyEndSell{}
+
 // enum Action{
 //     Sell {amount: i32},
 //     Buy {amount: i32},
@@ -36,22 +42,30 @@ fn main() {
     let _raw_data = RawData{_raw_data : String::from("data")};
     let _market_timeline: Vec<DataPoint> = parse_data(_raw_data);
 
-    let _strategy   = LoopBuySellStrategy {};
+    // let _strategy   = StategyLoopBuySell {};
+    let _strategy   = StrategyStartBuyEndSell {};
     let _account_starting_point = AccountBalancePoint{stocks_holding:0, cash_ammount:100000.0};
+    
     println!("current stocks holding is {} and amount of cash is {} rubles", &_account_starting_point.stocks_holding, &_account_starting_point.cash_ammount);
-    let _account_balance_history : Vec<AccountBalancePoint> = simulate(_strategy, _market_timeline, _account_starting_point);
+    
+    simulate(&_strategy, &_market_timeline, &_account_starting_point);
+    let _strategy   = StategyLoopBuySell {};
+    simulate(&_strategy, &_market_timeline, &_account_starting_point);
+    
 
-    println!("account is {:?}", _account_balance_history)
+
 }
+
 
 fn parse_data(_raw_data: RawData) ->Vec<DataPoint>{
     let data_point = DataPoint{ price : 5.3 };
     let data_array: Vec<DataPoint> = vec![data_point];
     return data_array
 }
-fn simulate <T: Strategy>(_strategy: T, _market_timeline : Vec<DataPoint>, starting_account: AccountBalancePoint) -> Vec<AccountBalancePoint>{
+
+fn simulate <T: Strategy + std::fmt::Debug>(_strategy: &T, _market_timeline : &Vec<DataPoint>, starting_account: &AccountBalancePoint){
     println!("market point is {}", _market_timeline[0].price);
-    let mut _account_balance_history : Vec<AccountBalancePoint>= vec![starting_account];
-    _account_balance_history
+    let  _account_balance_history : Vec<AccountBalancePoint>= vec![starting_account.clone()];
+    println!("{:?} resulted in {:?}",_strategy, _account_balance_history)
 }
 
