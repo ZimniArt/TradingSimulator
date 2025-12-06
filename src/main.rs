e59b843f361a1e2d17bb0882d5fcdd02fc026590
zimniart-admin <zimni.art@gmail.com>
2025-12-07 00:58:38 +0300

    before work on parser

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use strum::IntoEnumIterator;
 use strum_macros::EnumIter;
 use std::collections::HashMap;
-
+use chrono;
 
 #[derive(Debug)]
 //#[derive(Clone)]
@@ -16,7 +16,7 @@ enum Scenario{
     StartBuyEndSell,
     BuySellLoop,
 }
-#[derive(EnumIter, Debug)]
+#[derive(EnumIter, Debug,Eq, Hash, PartialEq)]
 enum Company{
     Yandex,
     Example,
@@ -33,30 +33,39 @@ impl Market{
     fn new()  -> Self{
         Self { companies_history: HashMap::new() }
     }
+    fn add_company(&mut self, _company :Company, _hisory: StocksHistory){
+        self.companies_history.insert(_company,_hisory);
+    }
 }
 
-// struct Candle{
-//     date: chrono ,
-//     price: f64,
-//     opening_price: f64,
-//     maximum_price: f64,
-//     minimum_price: f64,
-//     exchange_voplume: i32,
-//     price_exchange: f64,
-// }
+struct Candle{
+    date: chrono::NaiveDate ,
+    price: f64,
+    opening_price: f64,
+    maximum_price: f64,
+    minimum_price: f64,
+    exchange_voplume: f64,
+    price_exchange: f64,
+}
 fn main() { 
     let account_balance = AccountBalancePoint{stocks_holding:0, cash_ammount:100000.0};
 
-    for company in Company::iter(){
-        let mut _stocks_histoty= parse_data(&company);
-        test_all_scenarios(&_stocks_histoty, &account_balance);
-    }
+    let mut _stocks_histoty= parse_data();
+    test_all_scenarios(&_stocks_histoty, &account_balance);
     
 }
-fn parse_data(_company: &Company) -> Market {
-    let mut _stocks_histoty: Market = Market::new();
+fn parse_data() -> Market {
+    let mut _stocks_histoty: Market = Market::new(); 
+    for _copmany in Company::iter(){
+        let temp_history = StocksHistory::new();
+        _stocks_histoty.add_company(_copmany, temp_history);
+    }
     _stocks_histoty
 }
+
+fn load_data(_copmany:Company) {}
+fn clean_headers(){}
+
 fn test_all_scenarios(_stocks_histoty: &Market , account_balance: &AccountBalancePoint){
     for scenario in Scenario::iter(){
         match scenario {
