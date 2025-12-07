1ccbac802c5d002955f1ed329e881d1743d7a1d4
zimniart-admin <zimni.art@gmail.com>
2025-12-07 10:49:24 +0300

    CHAARTS!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use strum::IntoEnumIterator;
 use strum_macros::EnumIter;
+use core::f64;
 use std::{collections::HashMap};
 use chrono;
 use csv;
 use std::error::Error;
+use plotters::prelude::*;
 
 #[derive(Debug)]
 //#[derive(Clone)]
@@ -52,8 +54,11 @@ struct Candle{
 }
 fn main() { 
     let account_balance = AccountBalancePoint{stocks_holding:0, cash_ammount:100000.0};
-    let _aflt = parse_file("D:/2_projects/11_tradingSIm/Raw data/Прошлые данные - AFLT.csv");
-    println!("test {:?}", _aflt);
+    let _aflt = parse_file("D:/2_projects/11_tradingSIm/Raw data/Прошлые данные - AFLT.csv").unwrap();
+    let _afit_prices: Vec<f64> = _aflt.iter().map(|c|c.price).collect();
+
+    draw_chart(_afit_prices.len() as f64,_afit_prices , "D:/2_projects/11_tradingSIm/Graphics/flt_price.svg");
+   // println!("test {:?}", _aflt);
     let mut _stocks_histoty= parse_data();
     test_all_scenarios(&_stocks_histoty, &account_balance);
     
@@ -131,4 +136,36 @@ fn parse_file(path: &str) -> Result< Vec<Candle>, Box<dyn Error> >{
     }
 
     Ok(result)
+}
+
+
+fn draw_chart(_horizontal: f64, _vertical: Vec<f64>, output: &str) -> Result<(), Box<dyn Error>>  {
+    let root = SVGBackend::new(output, (1200,600)).into_drawing_area();
+    root.fill(&WHITE)?;
+
+     
+    
+    let (xmin,xmax)= (0.0,_horizontal);
+    let ymin = 0.0;
+    let ymax = _vertical.iter().cloned().fold(f64::NEG_INFINITY, f64::max);
+
+    let mut chart = ChartBuilder::on(&root)
+        .caption("chart example",("sans-serfi", 30))
+        .margin(20)
+        .x_label_area_size(40)
+        .y_label_area_size(60)
+        .build_cartesian_2d(xmin..xmax, ymin..ymax)?;
+
+    chart.configure_mesh().draw();
+
+    let data: Vec<(f64,f64)> = _vertical.iter().enumerate().map(|(i,v)| (i as f64, *v)).collect();
+    
+    chart
+        .draw_series(LineSeries::new(data, &RED))?
+        .label("some label")
+        .legend(|(x,y)| PathElement::new([(x,y), (x+20,y)], &RED));
+    chart.configure_series_labels().draw()?;
+    Ok(())
+    
+
 }
